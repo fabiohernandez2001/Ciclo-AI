63b23abe8430d8ea99443e077cf7fa7185e28a7d
fabio <fabiohmaru@gmail.com>
2025-10-20 02:27:15 +0100

    Creados clase Modelo: Item, Champion. Primera migracion a base de datos local sqlite realizada

--- backend/lol_navo/models.py
+++ backend/lol_navo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+
+
+class Champion(models.Model):
+    
---
+++ backend/lol_update/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LolUpdateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lol_update'
