ba3835a253bd90472b782d5bd5cb54e554ac4296
Willy <thewillyeb@gmail.com>
2025-10-25 03:02:50 +0100

    Basic functionalities of searchbar

--- frontend/src/hooks/useDebounceValue.jsx
+++ frontend/src/hooks/useDebounceValue.jsx
@@ -1,4 +1,9 @@
 import { useEffect, useState } from "react"
+/*
+    Custom hook that returns a debounced value.
+    The value will only update after the specified delay
+    has passed without the value changing.
+*/
 
 export default function useDebounceValue(value, delay = 300) {
     const [debounce, setDebounce] = useState(value)
