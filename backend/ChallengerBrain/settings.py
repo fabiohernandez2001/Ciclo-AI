4af95b796a4c7d3c49377fd861184aa09cc498c1
fabio <fabiohmaru@gmail.com>
2025-10-27 16:58:12 +0000

    (added) Configuración CORS para comunicarse con el frontend

--- backend/ChallengerBrain/settings.py
+++ backend/ChallengerBrain/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'lol_navo.apps.LolNavoConfig'
 ]
 
@@ -52,11 +53,15 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_WHITELIST = [
+    #'direccion del frontend',
+    
+]
 ROOT_URLCONF = 'ChallengerBrain.urls'
 
 TEMPLATES = [
