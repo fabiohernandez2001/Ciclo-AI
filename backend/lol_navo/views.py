d8484e22334953fe7ce6d3ab01dd8372a985a707
koldiii <ericlppgg@gmail.com>
2025-10-28 18:17:32 +0000

    Get_queryset de view

--- backend/lol_navo/views.py
+++ backend/lol_navo/views.py
@@ -1,15 +1,16 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-
 from .models import Champion
 from .serializers import ChampionSerializer
-
 
 class ChampionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Champion.objects.all().order_by("name")
     serializer_class = ChampionSerializer
     pagination_class = None
-    def get(self, request):
-        champion = Champion.objects.get(self.request.query_params.get("name"))
-        champion_serializer = ChampionSerializer(champion)
-        return Response(champion_serializer.data)
+#Hay que cambiar esto porque no esta serializado y to eso
+    def get_queryset(self):
+        queryset = Champion.objects.all().order_by("name")
+        name = self.request.query_params.get("name")  # obtiene ?name=Ahri
+        if name:
+            queryset = queryset.filter(name__icontains=name)  # busca coincidencias parciales
+        return queryset
